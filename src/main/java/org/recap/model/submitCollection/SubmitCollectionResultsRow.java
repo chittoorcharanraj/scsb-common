adbe6f36a77d93592bea8a79fcee143d25f161e8
DinakarCiber <dnaramala@ciber.com>
2021-05-29 10:37:34 +0530

    SC Exceptions changes

---
+++ src/main/java/org/recap/model/submitCollection/SubmitCollectionResultsRow.java
@@ -0,0 +1,18 @@
+package org.recap.model.submitCollection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class SubmitCollectionResultsRow {
+    private String itemBarcode;
+    private String customerCode;
+    private String owningInstitution;
+    private String reportType;
+    private String message;
+
+}
+
--- src/main/java/org/recap/service/SCSBService.java
+++ src/main/java/org/recap/service/SCSBService.java
@@ -4,7 +4,11 @@ import org.recap.ScsbCommonConstants;
 import org.recap.model.jpa.CollectionGroupEntity;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,5 +40,45 @@ public class SCSBService {
                 cgdCodesList.add(collectionGroupEntity.getCollectionGroupCode());
         }
         return cgdCodesList;
+    }
+    public Map<String, Date> dateFormatter(String fromDate, String toDate) throws Exception{
+        Map<String,Date> dateMap = new HashMap<>();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(ScsbCommonConstants.SIMPLE_DATE_FORMAT_REPORTS);
+        Date requestFromDate = simpleDateFormat.parse(fromDate);
+        Date requestToDate = simpleDateFormat.parse(toDate);
+        Date fromDateAfter = getFromDate(requestFromDate);
+        Date toDateAfter = getToDate(requestToDate);
+        dateMap.put("fromDate",fromDateAfter);
+        dateMap.put("toDate",toDateAfter);
+        return  dateMap;
+    }
+    /**
+     * For the given date this method will add the start time of the day.
+     *
+     * @param createdDate the created date
+     * @return the from date
+     */
+    public Date getFromDate(Date createdDate) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(createdDate);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        return cal.getTime();
+    }
+
+    /**
+     * For the given date this method will add the end time of the day.
+     *
+     * @param createdDate the created date
+     * @return the to date
+     */
+    public Date getToDate(Date createdDate) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(createdDate);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        return cal.getTime();
     }
 }
